ea14e4e5383c7cd1147905abac086c0926184e23
agamrat <amber.gamrat@mail.mcgill.ca>
2013-09-20 16:04:08 -0400

    first try

---
+++ tree.h
@@ -0,0 +1,32 @@
+#ifndef TREE_H
+#define TREE_H
+ 
+typedef struct EXP {
+  int lineno;
+  enum {idK,intconstK,timesK,divK,plusK,minusK,modK} kind;
+  union {
+    char *idE;
+    int intconstE;
+    struct {struct EXP *left; struct EXP *right;} timesE;
+    struct {struct EXP *left; struct EXP *right;} divE;
+    struct {struct EXP *left; struct EXP *right;} plusE;
+    struct {struct EXP *left; struct EXP *right;} minusE;
+    struct {struct EXP *left; struct EXP *right;} modE;
+  } val;
+} EXP;
+ 
+EXP *makeEXPid(char *id);
+
+EXP *makeEXPintconst(int intconst);
+
+EXP *makeEXPtimes(EXP *left, EXP *right);
+
+EXP *makeEXPdiv(EXP *left, EXP *right);
+
+EXP *makeEXPplus(EXP *left, EXP *right);
+
+EXP *makeEXPminus(EXP *left, EXP *right);
+
+EXP *makeEXPmod(EXP *left, EXP *right);
+
+#endif /* !TREE_H */
