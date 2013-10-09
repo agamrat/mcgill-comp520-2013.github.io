0eeb84e115d5f1d9c055fa2baacfdd9241be4e19
agamrat <amber.gamrat@mail.mcgill.ca>
2013-10-09 19:07:48 -0400

    added mutually recursive tree structure

--- WIGA3/tree.ml
+++ WIGA3/tree.ml
@@ -1,14 +1,14 @@
 
+type lineNumber = int
 
-type node =
+type abstractTree = (lineNumber * node) 
+
+and node =
 	Plus of (abstractTree * abstractTree)
 	| Number of int
 	| String of string
 
-type lineNumber = int
 
-type abstractTree =
-	(lineNumber * node) 
 
 
 
